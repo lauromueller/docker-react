cd904a5f46f9564ecb41d55583ee72a02cda0bb4
Lauro Fialho Müller <contact@lauromueller.com>
2021-01-11 17:54:18 +0100

    small change to app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          One more wow .
+          I was changed!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          We are in the feature branch.
         </a>
       </header>
     </div>
